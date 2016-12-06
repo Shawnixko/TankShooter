94ae5186e6a6bf42e901a9ecd981b75cba41fbf7
Shawnixko <Shawn.Hickens@mail.bcu.ac.uk>
2016-12-06 12:07:50 +0000

    Small XBOX Controller Support

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -15,7 +15,7 @@ public class Shooting : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("Fire1"))
         {
             fireLazer();
         }
